405f69a30bcc41bbe3b43a977fc1cb139d3ea2dd
Asif <asif13199615@gmail.com>
2021-10-14 00:05:04 +0530

    Skeleton of our jquery app is done

--- jquery/common.js
+++ jquery/common.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $("#btn-click").click(function () {
     alert("you clicked !!!!");
   });
+
+  $("#tabs").tabs();
+  $("ul").sortable({ axis: "x", containment: "#tabs" });
+  $("ol").sortable({ axis: "y", containment: "#tabs" });
 });
