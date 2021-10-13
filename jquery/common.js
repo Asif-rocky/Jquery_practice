2bfd97e476ab62485c1ec38e8fa029338b75820f
Asif <asif13199615@gmail.com>
2021-10-14 01:08:29 +0530

    Added a new challenge button and configured the style of li elements.

--- jquery/common.js
+++ jquery/common.js
@@ -6,4 +6,25 @@ $(document).ready(function () {
   $("#tabs").tabs();
   $("ul").sortable({ axis: "x", containment: "#tabs" });
   $("ol").sortable({ axis: "y", containment: "#tabs" });
+  $("#btn-challenge").click(function () {
+    $("#button-dialog").dialog({
+      width: 400,
+      resizable: false,
+      modal: true,
+      buttons: {
+        "Add a new challenge": function () {
+          var inptValue = $("#inpt").val();
+          var liHtml =
+            "<li><a href='#" + inptValue + "'>" + inptValue + "</a></li>";
+          $("#tabs-ul").append(liHtml);
+          $("<ol id='" + inptValue + "'></ol>").appendTo("#tabs");
+          $("#tabs").tabs("refresh");
+          var tabCount = $("#tabs .ui-tabs-nav li").length;
+          $("#tabs").tabs("option", "active", tabCount - 1);
+          $("#inpt").val("");
+          $(this).dialog("close");
+        },
+      },
+    });
+  });
 });
