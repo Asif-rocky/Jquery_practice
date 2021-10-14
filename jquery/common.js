c5c2be37264202a3a63f647b4f0c3615c1d47f51
Asif <asif13199615@gmail.com>
2021-10-14 12:29:51 +0530

    added a new task button and modified few features using jquery

--- jquery/common.js
+++ jquery/common.js
@@ -7,8 +7,16 @@ $(document).ready(function () {
   $("ul").sortable({ axis: "x", containment: "#tabs" });
   $("ol").sortable({ axis: "y", containment: "#tabs" });
   $("#btn-challenge").click(function () {
+    $("#tabs").tabs({
+      activate: function (event, ui) {
+        ui.newPanel.css("display", "flex");
+      },
+    });
+    $("#tabs").css("height", "auto");
+    $("#button-dialog").html("");
+    dialogCreate("challenge");
     $("#button-dialog").dialog({
-      width: 400,
+      width: 450,
       resizable: false,
       modal: true,
       buttons: {
@@ -27,4 +35,39 @@ $(document).ready(function () {
       },
     });
   });
+
+  $("#btn-task").click(function () {
+    var currentTab = $("ul> .ui-tabs-active").attr("aria-controls");
+    var tabId = "#" + currentTab;
+    $("#button-dialog").html("");
+    dialogCreate("task");
+    $("#button-dialog").dialog({
+      width: 400,
+      resizable: false,
+      modal: true,
+      buttons: {
+        "Add a new task/challenge": function () {
+          var inptValue = $("#inpt").val();
+          var liHtml = "<li>" + inptValue + "</li>";
+          $(tabId).append(liHtml);
+          $("#tabs").tabs("refresh");
+          var tabCount = $("#tabs .ui-tabs-nav li").length;
+          $("#tabs").tabs("option", "active", tabId);
+          $("#inpt").val("");
+          $(this).dialog("close");
+        },
+      },
+    });
+  });
+
+  function dialogCreate(name) {
+    var name = name + " " + "Name";
+    var html =
+      "<label for=name>" +
+      name +
+      ":</label>" +
+      "<input id='inpt' type='text' style='margin: 2px; margin-left:10px;'>";
+    $("#button-dialog").append(html);
+    $("#button-dialog").prop("title", "Enter the new " + name);
+  }
 });
